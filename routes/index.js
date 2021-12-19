b10d1517461bdf653e83c6cf3866e4d6b5932998
emeraldtanmeiing <emeraldtanmeiing@gmail.com>
2021-12-19 23:57:48 +0800

    feat: author

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+//healthcheck route
 router.get('/', (req, res) => {
-    res.render("index");
+    res.render('index');
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -5,12 +5,15 @@ if(process.env.NODE_ENV !== 'production'){
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
 
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true});
@@ -19,7 +22,9 @@ db.on('error', err => console.error(err))
 db.once('open', () => console.log('Connected to database.'));
 
 
-const routers = require('./routes/index');
-app.use('/', routers);
+const indexRouter = require('./routes/index');
+const authorsRouter = require('./routes/authors');
+app.use('/', indexRouter);
+app.use('/authors', authorsRouter);
 
 app.listen(process.env.PORT || 3000);
